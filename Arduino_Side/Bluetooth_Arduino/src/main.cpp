d1c6786ec20b851d7dd9779115428d47239c365a
mcd005 <j.pmcdonald005@gmail.com>
2021-08-11 22:21:12 +0100

    Add function to read from Serial0

--- Arduino_Side/Bluetooth_Arduino/src/main.cpp
+++ Arduino_Side/Bluetooth_Arduino/src/main.cpp
@@ -10,9 +10,17 @@ void setup()
 void loop()
 {
     // read from port 1, send to port 0:
+    // If this is the only function in the loop, it essential just iterators over the Serial1 buffer and prints it to the console
     if (Serial1.available())
     {
         char inByte = Serial1.read(); // Read takes a single byte off the top of the buffer. Serial.available() would return 0 if called again
         Serial.write(inByte); // Writes binary data to the SerialPort. Will convert from int to ASCII char quite happily (hence prints NULL if you try to print 0)
+    }
+
+    // read from port 0, send to port 0:
+    if (Serial.available())
+    {
+        String inPhrase = Serial.readString();
+        Serial.println(inPhrase);
     }
 }
