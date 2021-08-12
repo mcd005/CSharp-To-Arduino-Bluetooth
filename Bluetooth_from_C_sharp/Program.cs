a9c7c0821b1a3f0588fe1cb2efdf8c531add368d
mcd005 <j.pmcdonald005@gmail.com>
2021-08-12 18:18:40 +0100

    Non-working BT C# code

---
+++ Bluetooth_from_C_sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Bluetooth_from_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            serialPort1.PortName = "COM9";
+            serialPort1.baudRate = 9600;
+            try
+            {
+                if (!serialPort1.IsOpen) serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+            String messageToSend = "Hello from C#!";
+            serialPort1.Write(messageToSend);
+            serialPort1.Close();
+        }
+    }
+}
